13f570a051296cc4540c2874594e0e2c769c86c7
Shibu S <shibus29@in.ibm.com>
2019-12-31 10:45:32 +0530

    all origin allow in header

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 	
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // default route
 app.get('/', function (req, res) {
